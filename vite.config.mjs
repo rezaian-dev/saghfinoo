40c18c7f41efcd3fdd3ae2117da2366dac73bf30
rezaian-dev <rezaian.developer@gmail.com>
2025-01-12 15:49:16 +0330

    added project base structure

---
+++ vite.config.mjs
@@ -0,0 +1,16 @@
+// vite.config.mjs
+
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
+});
